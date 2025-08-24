fe5e7ef1bffb4a6bda971e38cb4f7eaddd2fbc56
ry-murashige <ry-murashige@kddi-agdc.com>
2025-08-24 13:05:22 +0900

    ログインフォームを作成

--- my-auth-app/src/app/page.tsx
+++ my-auth-app/src/app/page.tsx
@@ -2,7 +2,65 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>認証についてのWebApp</h1>
-    
-      );
+    <>
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div>
+          <h2 className="flex justify-center text-xl">Sign in to your account</h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-gray-100"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  type="email"
+                  id="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white/5 px-3 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm/6 front-medium text-gray-100"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-400 hover:text-indigo-300"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  type="password"
+                  id="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md bg-white/5 px-3 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500"
+                />
+              </div>
+            </div>
+            <div>
+              <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2">SIgn in</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
