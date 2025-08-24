d7c49bdca125b3ccae260ea3682ef1970cf4c240
ry-murashige <ry-murashige@kddi-agdc.com>
2025-08-24 18:59:27 +0900

    アカウント作成ページを追加し、ホームページにアカウント作成ボタンを追加

---
+++ src/app/create-account/page.tsx
@@ -0,0 +1,84 @@
+const createAccount = () => {
+  return (
+    <>
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div>
+          <h2 className="flex justify-center text-xl">Create Account</h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm/6 font-medium text-gray-100"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                required
+                autoComplete="name"
+                className="block w-full rounded-md bg-white/5 px-3 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm/6 front-medium text-gray-100"
+              >
+                Email address
+              </label>
+              <input
+                type="email"
+                id="email"
+                autoComplete="email"
+                className="block w-full rounded-md bg-white/5 px-3 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500"
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm/6 front-medium text-gray-100"
+              >
+                New Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                required
+                autoComplete="current-password"
+                className="lock w-full rounded-md bg-white/5 px-3 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500"
+              />
+              <label
+                htmlFor="password"
+                className="block text-sm/6 front-medium text-gray-100"
+              >
+                Password comparison
+              </label>
+              <input
+                type="password"
+                id="password"
+                required
+                autoComplete="current-password"
+                className="lock w-full rounded-md bg-white/5 px-3 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500"
+              />
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2"
+              >
+                Create Account
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default createAccount;
